4b5f032da9ac340699000472ac2fe6d9fa5162e2
Juan Ocampo <desarrollojuseca@gmail.com>
2019-11-15 11:05:56 -0500

    starting main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+void loop(void)
+{
+	char *line, **args;
+	int status;
+
+	printf("#cisfun$ \n");
+	do
+	{
+		line = read();
+	} while (status);
+	
+}
+
+int main()
+{
+	loop();
+	return 0;
+	
+}
