c4fe830ef68e8957bf9c92aef981e3085ea2085c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-11-22 15:23:05 +0000

    update the header shell

--- shell.h
+++ shell.h
@@ -5,7 +5,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
-char *split_str(char *str, const char *delim);
 
+char **split_str(char *str, const char *delim);
+int _fork(char **temp_split);
+
+/*helpers*/
+size_t tokenlen(const char *str)
+char *_strcpy(char *dest, char *src)
 
 #endif
