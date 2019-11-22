40954f58d8c443360545aa19ecfaea279a94f630
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-11-22 15:22:14 +0000

    update the file main

--- main.c
+++ main.c
@@ -1,21 +1,48 @@
 #include "shell.h"
-
-void loop(void)
+/**
+ * main - void
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
 {
-	char *line, **args;
-	int status;
+int i;
+size_t buff_size = 10;
+ssize_t printer = 1;
+pid_t pid;
+char **temp_split = NULL;
+char *buffer = malloc(buff_size);
 
-	printf("#cisfun$ \n");
-	do
+	if (buffer == NULL)
 	{
-		line = read();
-	} while (status);
-	
+		perror("Failed to malloc memory");
+		free(buffer);
+		exit(98);
+	}
+	while (printer > 0)
+	{
+		printf("$ ");
+		printer = getline(&buffer, &buff_size, stdin);
+		if (printer == -1)
+			break;
+		printf("%s", buffer);
+		temp_split = split_str(buffer, " ");
+
+		pid = _fork(temp_split);
+		i = 0;
+		while (temp_split[i])
+			i++;
+		while (i >= 0)
+		{
+			free(temp_split[i]);
+			i--;
+		}
+		free(temp_split);
+	}
+	free(buffer);
+return (0);
 }
 
-int main()
-{
-	loop();
-	return 0;
-	
-}
+
+
