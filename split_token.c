cb1e73ebe719155160972f64961613c13b3aa063
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-11-22 15:24:22 +0000

    separate the functions

---
+++ split_token.c
@@ -0,0 +1,56 @@
+#include "shell.h"
+/**
+ *split_str - Function to copy
+ *@str: edited variable
+ *@delim: edited variable
+ *Return: always 0
+*/
+char **split_str(char *str, const char *delim)
+{
+	int len = 0, numLetters = 0, i =0, new;
+	char **token_list = NULL;
+	char *tempStr = NULL;
+	char *token = NULL;
+
+	tempStr = malloc(sizeof(str));
+	if (tempStr == NULL)
+	{
+		perror ("Error");
+		free(tempStr);
+	}
+	/*letters count*/
+	while (str[len] != '\0')
+	{
+		if (str[len] == '\n')
+			str[len] = '\0';
+		if (str[len] != delim[0])
+			numLetters++;
+		len++;
+	}
+	tempStr = _strcpy(tempStr, str);
+	printf("str new is: %s", tempStr);
+	token_list = malloc((sizeof(char *) * (numLetters + 1)));
+	
+	if (token_list == NULL)
+	{
+		perror ("Error");
+		free(token_list);
+	}
+	token = strtok(tempStr, delim);
+	while (token)
+	{
+		token_list[i] = malloc(sizeof(char) * (strlen(token) + 1));
+		if (token_list[i] == NULL)
+		{
+			for(new = i; new >= 0; new--)
+				free(token_list[i]);
+			free(token_list);
+		}
+		_strcpy(token_list[i],token);
+		i++;
+		token = strtok(NULL, delim);
+	}
+	token_list[i] = NULL;
+	free(tempStr);
+	return (token_list);
+}
